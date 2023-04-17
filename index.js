ae7f69bff351742a4ef2a16afee573391f4550ca
benqy <hmjlr123@gmail.com>
2023-04-17 13:43:29 +0800

    word general

---
+++ index.js
@@ -0,0 +1,8 @@
+const fse = require('fs-extra')
+
+const str = fse.readFileSync('./English-resources.assets.txt', 'utf8')
+
+const arr = str.split('\r\n')
+const values = arr.map(t => t.split('=')[1])
+console.log(values)
+fse.writeFileSync('./words.txt', values.join('\n'))
